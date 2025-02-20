d56f4aa3965504f83700c6d66e665782bbf10072
Kudzeri <kudzeri@gmail.com>
2025-02-20 22:58:36 +0500

    added portfolio total price

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,18 +1,33 @@
-import {Layout} from "antd";
+import { Layout, Typography } from "antd";
+import { useCrypto } from "../../context/CryptoContext";
 
 const contentStyle = {
-    textAlign: 'center',
-    minHeight: 'calc(100vh - 60px)',
-    color: '#fff',
-    backgroundColor: '#001529',
-    padding: '1rem',
+  textAlign: "center",
+  minHeight: "calc(100vh - 60px)",
+  color: "#fff",
+  backgroundColor: "#001529",
+  padding: "1rem",
 };
 
 export const AppContent = () => {
-    return (
-        <Layout.Content style={contentStyle}>
-            Content
-        </Layout.Content>
+  const { assets, crypto } = useCrypto();
+  const cryptoPriceMap = crypto.reduce((acc, c) => {
+    acc[c.id] = c.price;
+    return acc;
+  }, {});
 
-    )
-}
+  return (
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={3} style={{ textAlign: "left", color: "#fff" }}>
+        Portfolio:{" "}
+        {assets
+          .map((asset) => {
+            return asset.amount * cryptoPriceMap[asset.id];
+          })
+          .reduce((acc, v) => (acc += v), 0)
+          .toFixed(2)}
+        $
+      </Typography.Title>
+    </Layout.Content>
+  );
+};
